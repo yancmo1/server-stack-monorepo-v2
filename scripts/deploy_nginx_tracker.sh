856a9d11fc700eede77c2a504e52c363c8c0887c
yancmo1 <yancmo@gmail.com>
2025-08-10 21:59:25 -0500

    Deploy: 2025-08-10 21:59:25 - 1 file(s) updated

--- scripts/deploy_nginx_tracker.sh
+++ scripts/deploy_nginx_tracker.sh
@@ -9,12 +9,17 @@ LOCAL_CONF="$(dirname "$0")/../deploy/nginx/yancmo.xyz.conf"
 REMOTE_CONF="/etc/nginx/sites-available/yancmo.xyz.conf"
 
 # Copy the config file to the server
-scp "$LOCAL_CONF" "$REMOTE:$REMOTE_CONF"
+	   # Copy the config file to the user's home directory on the server
+	   scp "$LOCAL_CONF" "$REMOTE:~/yancmo.xyz.conf"
+
+	   # Move the config into /etc/nginx/sites-available/ with sudo, symlink, test, and reload
+	   ssh "$REMOTE" "sudo mv ~/yancmo.xyz.conf /etc/nginx/sites-available/yancmo.xyz.conf && \
+		   sudo ln -sf /etc/nginx/sites-available/yancmo.xyz.conf /etc/nginx/sites-enabled/yancmo.xyz.conf && \
+		   sudo nginx -t && sudo systemctl reload nginx"
 
 # Symlink to sites-enabled if not already
-ssh "$REMOTE" "sudo ln -sf /etc/nginx/sites-available/yancmo.xyz.conf /etc/nginx/sites-enabled/yancmo.xyz.conf"
 
 # Test nginx config and reload
-ssh "$REMOTE" "sudo nginx -t && sudo systemctl reload nginx"
 
 echo "Nginx config deployed and reloaded on $REMOTE."
+	   echo "Nginx config deployed and reloaded on $REMOTE."
